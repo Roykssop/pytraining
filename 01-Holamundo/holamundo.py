00d6f7f521a4bec5c821ebfd190301c2bc2cc0be
herna <hernan14@gmail.com>
2020-06-29 18:43:53 -0300

    Sec3 - Variables | LEC[9,10]

--- 01-Holamundo/holamundo.py
+++ 01-Holamundo/holamundo.py
@@ -1,1 +1,7 @@
+# Comment de 1 linea
 print("Hola mundo!")
+
+"""
+Comment multilínea
+print("Esto es un comment tmb");
+"""
---
+++ 02-variables-y-tipos/vartypes.py
@@ -0,0 +1,18 @@
+cadena = "Hello"
+cadena2 = "People"
+numero = 1
+flotante = 1.5
+
+
+print(cadena)
+print(numero)
+print(flotante)
+
+# Concatenación
+concatena2 = cadena + " " + cadena2
+
+# Tipos de logs (print)
+print(concatena2)
+print(f"1er cadena {cadena},  2da cadena {cadena2}, numero {numero}")
+print("1er cadena {},  2da cadena {}, numero {}".format(cadena,cadena2,numero))
+print(cadena,cadena2,numero)
