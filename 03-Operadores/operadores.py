39ae5027bb3095849cdb9ec43f1444c75a75c07a
herna <hernan14@gmail.com>
2020-06-30 18:05:20 -0300

    Sec4 - Operadores  | LEC[14,15,16]

---
+++ 03-Operadores/operadores.py
@@ -0,0 +1,23 @@
+# Operaciones numericas
+num1 = 10
+num2 = 5
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+resto = num1 % num2 
+
+print("suma: {}, resta:{}, multiplicacion:{}, division: {}, resto: {} ".format(suma, resta,multiplicacion,division,resto))
+
+# Operadores de asignacion
+name = "Henry"
+num2 += num1 
+
+print(f"nombre: {name} , Numero: {num2} ")
+
+# Operadores incremento decremento
+num2 = num2 +1
+num2 += 1
+num1 -= 1
+
+print(f"Decremento en numero 1: {num1} , Incremento en numero 2: {num2}")
