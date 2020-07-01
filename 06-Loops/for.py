0be8444edea6d9579a50628b033f71bb2fa6336e
herna <hernan14@gmail.com>
2020-07-01 19:16:35 -0300

    Sec7 - For Loop | LEC[24,25]

---
+++ 06-Loops/for.py
@@ -0,0 +1,25 @@
+"""
+  Bucle for
+"""
+
+contador = 0
+
+for contador in range(1,11):
+  print(f"Vamos por el indice {contador}")
+
+"""
+  Ejer: Tabla de multiplicar
+"""
+
+numTabla = int(input("Que tabla de multiplicar quiere ver?"))
+counting = 0
+
+for counting in range(1,11):
+  print(f"El número elegido fue {numTabla} x {counting} = {counting*numTabla} ")
+else:
+  print("Final")
+
+
+
+
+
