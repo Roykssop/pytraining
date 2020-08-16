27ca93c94368a40ae5c60bc5be509b6c2a1d1740
Roykssop <herna14@gmail.com>
2020-08-16 13:00:42 -0300

    Sec14 - Leer ficheros | LEC70

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -2,6 +2,29 @@ from io import open
 import pathlib  
 
 ruta = str(pathlib.Path().absolute()) + "/fichero.txt"
-archivo = open("fichero.txt","a+")
 
-archivo.write("Escribiendo un archivo... \n")
+# Crear archivo
+# archivo = open("fichero.txt","a+")
+
+# Escribir archivo
+# archivo.write("Escribiendo un archivo... \n")
+
+# Leer archivo
+archivo = open("fichero.txt","r")
+# .read devuelve un string
+#lectura = archivo.read()
+
+# Recorrer cada caracter del texto
+#for character in lectura:
+#  print(character)
+
+# Leer por lineas y guardarlo como list
+ficheroEnLista = archivo.readlines()
+
+for linea in ficheroEnLista:
+  line = linea.split(" ")
+  for word in line:
+    print(word)
+
+# Cerrar archivo
+archivo.close()
