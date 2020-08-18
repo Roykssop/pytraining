4a70487916885234b3c8d5342429de39ffb86f23
Roykssop <herna14@gmail.com>
2020-08-18 08:04:59 -0300

    Sec15 - Manejando multiples excepciones  | LEC77

--- 15-manejo-errores/main.py
+++ 15-manejo-errores/main.py
@@ -3,6 +3,9 @@
   susceptible a fallos/errores
 """
 
+"""
+#  Manejo básico de excepciones
+
 try:
   numero = input("Ingrese un número: ")
   numero2 = input("Ingrese otro número: ")
@@ -13,3 +16,21 @@ except:
   print(f"Error en la ejecución del programa")
 finally:
   print(f"Finalizó el programa")
+"""
+
+"""
+  Manejo de múltiples errores
+"""
+
+# Imprimo nombre del tipo
+print(type(9).__name__)
+
+try:
+  numero = int(input("Ingrese un número para elevarlo al cuadrado: "))
+  print(f"La multiplicación final es:"+str(numero*numero) )
+except TypeError:
+  print("Debes convertir tus cadenas a enteros en el código")
+except ValueError:
+  print("Introduce un número correcto!")
+except Exception as e:
+  print("Ha ocurrido unn error", type(e).__name__)
