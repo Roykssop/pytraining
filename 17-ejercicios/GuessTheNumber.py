2708aee79da25e001eaf67f9b1a664798589b9a2
Roykssop <herna14@gmail.com>
2020-08-19 11:00:41 -0300

    Sec17 - Ejercicio Guess the number, metodos privados | LEC

--- 17-ejercicios/GuessTheNumber.py
+++ 17-ejercicios/GuessTheNumber.py
@@ -10,24 +10,24 @@ class GuessTheNumber:
     self.__topNumber = topNumber
     self.__numberToGuess = random.randint(1,topNumber)
 
-  def isWinner(self,num):
+  def __isWinner(self,num):
     return num == self.__numberToGuess
 
-  def getLeftAttempts(self,currentAttempt):
+  def __getLeftAttempts(self,currentAttempt):
     return self.__attempts - currentAttempt
 
   def startGame(self):
     for i in range(self.__attempts):
       while True:
         try:  
-          numero=int(input(f"Adivine el número de 1 a { self.__topNumber }, tiene { self.getLeftAttempts(i) } intentos: "))
+          numero=int(input(f"Adivine el número de 1 a { self.__topNumber }, tiene { self.__getLeftAttempts(i) } intentos: "))
           if(numero < 1 or numero > self.__topNumber):
             raise ValueError("Ingrese un numero dentro del rango de 1 a 20 por favor")   
           break
         except ValueError:
           print("Ingresa un número correcto")
 
-      if(self.isWinner(numero)):
+      if(self.__isWinner(numero)):
         self.__resultado = "Adivinaste el número!"
         break
 
