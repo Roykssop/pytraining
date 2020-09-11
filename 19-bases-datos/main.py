4d2c2a2ed8d71947720def05daf784ed422c4fec
Roykssop <herna14@gmail.com>
2020-09-11 10:49:43 -0300

    Sec19 - Mysql Insert y Select | LEC[97,98]

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -10,12 +10,57 @@ database = mysql.connector.connect(
 # Hubo conexión?
 print(database)
 
-cursor = database.cursor()
+# Cursor
+cursor = database.cursor(buffered=True)
 
+# Crear base de datos
 cursor.execute("CREATE DATABASE IF NOT EXISTS pytraining")
 cursor.execute("SHOW DATABASES")
 
+"""
 for db in cursor:
   print(db)
+"""
 
+# Crear tabla
+cursor.execute("USE pytraining")
+cursor.execute("""CREATE TABLE IF NOT EXISTS vehiculos (
+  id int(10) auto_increment not null,
+  marca varchar(40) not null,
+  modelo varchar(40) not null,
+  precio float(10,2) not null,
+  CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+)
+""")
 
+"""
+cursor.execute("SHOW TABLES")
+
+for table in cursor:
+  print(table)
+"""
+
+#  Insert registro
+#cursor.execute("INSERT INTO vehiculos VALUES (null,'Ford','Fiesta',900000)")
+vehiculos = [
+  ('Renault','Kangoo',750000),
+  ('Citroen','C4',750000)
+]
+
+#cursor.executemany("INSERT INTO vehiculos VALUES (null,%s,%s,%s)",vehiculos)
+database.commit()
+
+# Select Registros
+cursor.execute("SELECT * FROM vehiculos")
+result = cursor.fetchall()
+
+print("======= LISTA DE VEHICULOS =======")
+for vehiculo in result:
+  print(vehiculo[1], vehiculo[3])
+
+# Select primer registro
+cursor.execute("SELECT * FROM vehiculos")
+result = cursor.fetchone()
+
+print("======= PRIMER VEHICULO =======")
+print(result[1],result[3])
