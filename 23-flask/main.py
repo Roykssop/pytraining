03dfa24a862e116db256e3951a5dd9476f6f2b7c
Roykssop <herna14@gmail.com>
2020-09-14 10:37:59 -0300

    Sec23 - Flask: Parámetros opcionales en rutas  | LEC[300,301]

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 # Asociamos el route con la función inmediatamente definida
 def index():
-  return "Aprendiendo Flask"
+  return "Aprendiendo Flask" 
 
 @app.route('/contacto')
 # Es válido retornar html
 def contacto():
   return "<h1> Página de contacto</h1>"  
+
+@app.route('/informacion/')
+@app.route('/informacion/<string:nombre>')
+@app.route('/informacion/<string:nombre>/<int:dato>')
+def informacion(nombre = None,dato = None):
+  ret = ""
+
+  if(nombre != None and dato != None):
+    ret = f"Parámetros: El nombre es {nombre}, y el dato requerido: {dato}"
+
+  return ret
 
 @app.route('/lenguajes-de-programacion')
 # Es válido retornar html
